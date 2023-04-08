c3ad74dff0cb6da38dcf32759d9388cf9036bcbc
GovindKota <govinda.kota@500apps.com>
2023-04-08 13:44:20 +0530

    Blocks

---
+++ components/blocks/List.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="px-4 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+        <p class="mt-2 text-sm text-gray-700">
+          A list of all the users in your account including their name, title,
+          email and role.
+        </p>
+      </div>
+    </div>
+    <div class="mt-8 flow-root">
+      <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div
+            class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg"
+          >
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Category
+                  </th>
+                  <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span class="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                <tr
+                  v-for="(user, index) in blocksData.data._rawValue"
+                  :key="index"
+                >
+                  <td
+                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"
+                  >
+                    {{ user.name }}
+                  </td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {{ user.category }}
+                  </td>
+                  <td
+                    class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6"
+                  >
+                    <PencilSquareIcon
+              @click="emit('prefillData', user, index)"
+              class="h-5 w-5 mr-3"
+              aria-hidden="true"
+            ></PencilSquareIcon>
+            <TrashIcon
+              @click=" emit('deleteRecord', user, index),
+                        (open = true)
+                    "
+              class="h-5 w-5 "
+              aria-hidden="true"
+            ></TrashIcon>
+                  </td>
+
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { defineProps,defineEmits } from "vue";
+import { XMarkIcon ,TrashIcon, PencilSquareIcon,CheckIcon} from "@heroicons/vue/24/outline";
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+
+const props = defineProps({
+  blocksData: {
+    type: Object,
+    default: () => {},
+  },
+});
+const open= ref(false)
+const emit = defineEmits(['prefillData'])
+</script>
