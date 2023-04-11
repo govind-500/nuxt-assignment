7da112030378f51dc8dd4335b1d2c3215d68da9d
GovindKota <govinda.kota@500apps.com>
2023-04-11 13:54:54 +0530

    Leaflet component

---
+++ components/leaflet/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <LeafletList
+      :center="center"
+      :markerLatLng="markerLatLng"
+      :zoom="zoom"
+      :mapViewUrl="mapViewUrl"
+    ></LeafletList>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+// declaring variables are passing through the props
+
+const center = ref([17.4484363, 78.3741361]);
+const markerLatLng = ref([17.4484363, 78.3741361]);
+const zoom = ref(13);
+const mapViewUrl = ref("https://{s}.tile.osm.org/{z}/{x}/{y}.png");
+</script>
---
+++ components/leaflet/Popup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--preson details starts here -->
+    <div class="flex" v-for="person in personDetails" :key="person.name"> 
+      <img
+        class="mt-3 h-10 w-10 rounded-full"
+        :src="person.imageUrl"
+        width="500"
+        height="600"
+      />
+      <div class="ml-2">
+        <p>{{person.name}}</p>
+      </div>
+    </div>
+    <!--preson details ends here -->
+  </div>
+</template>
+<script lang='ts' setup>
+
+const props = defineProps({
+  /**
+   * used for person details displayed in popup
+   */
+  personDetails:{
+    type:Array,
+    default:()=>[]
+  }
+})
+</script>
